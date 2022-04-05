d4b678f3d8b2dc41555221826870b74309745ebb
suatatan <suatatan@gmail.com>
2022-04-05 18:37:13 +0300

    Models defined (draft) and registered

--- eye/eyeapp/admin.py
+++ eye/eyeapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from eyeapp.models import Application, Event
+admin.site.register(Application)
+admin.site.register(Event)
--- eye/eyeapp/models.py
+++ eye/eyeapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from jsonfield import JSONField
+import uuid
 
-# Create your models here.
+class Event(models.Model):
+    session_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    category =  models.CharField(max_length=70)
+    name = models.CharField(max_length=70)
+    data= JSONField()
+    timestamp = models.DateTimeField(auto_now=True)
+    
+def __str__(self):
+        return self.session_id
+
+class Application(models.Model):
+    name=models.CharField(max_length=30)
+    token=models.CharField(max_length=30)
+    sessions=models.ManyToManyField(Event)
+
+
+
+#python 2 kullanıcıları için def __unicode__(self) 
+    def __str__(self):
+        return '%s %s'%(self.isim,self.soyisim)
